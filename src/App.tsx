fe6f473f4ecf24b0adc3856b84ad5d16f503306a
Blaze-trio <64141242+Blaze-trio@users.noreply.github.com>
2025-05-25 23:53:51 +0300

    three,js added 3D models and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import{Route,BrowserRouter as Router,Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import {Home,About,Contact,Projects} from './pages';
 const App = () => {
   return (
-    <div>App
-          <h1 className="text-3xl font-bold underline">    Hello world!  </h1>
-    </div>
-
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/island'
+const Home = () => {
+  return (
+    <section className="w-full h-screen relative">
+        <div className="absolute top-28 left-0 right-0 z-10 flex intems-center justify-center">
+            POPup
+        </div>
+        <Canvas className="w=full h-full bg-transparent"
+            camera={{ near: 0.1, far: 1000}}>
+            <Suspense fallback={<Loader/>}>
+                <Island />
+            </Suspense>
+        </Canvas>
+    </section>
+  )
+}
+
+export default Home
